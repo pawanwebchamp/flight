e28ad85c3ce8ffa6dcc8a6a43bc49cca971cb807
pawan <pawanwebchamp@gmail.com>
2018-04-08 09:57:03 +0530

    added filters

--- app/js/app.module.js
+++ app/js/app.module.js
@@ -1,4 +1,4 @@
-var app = angular.module('flightApp', ['ngRoute']);
+var app = angular.module('flightApp', ['ngRoute', 'ngMessages', 'ngMaterial']);
 // app.component('myComponent', {
 //  templateUrl: 'flight/flight-list.html',
 //  controller: ('flightController', function($scope){
@@ -8,20 +8,39 @@ var app = angular.module('flightApp', ['ngRoute']);
 app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
     $locationProvider.hashPrefix('!');
     $routeProvider.when('/flight', {
-        templateUrl: '/flight/flight-list.html'
+        templateUrl: 'app/flight/flight-list.html'
     }).
-        otherwise({ redirectTo: 'index.html' });
+        otherwise({ redirectTo: '/flight' });
 }]);
 
 app.controller('flightController', ['$scope', '$http', function ($scope, $http) {
+    var ctrl = this;
     $scope.SearchFlight = function () {
+        
         $scope.searchResult = true;
         $scope.loading = true;
-        $http.get('flight/flight.json')
+        $http.get('app/flight/flight.json')
             .then(function (response) {
-                $scope.flights = response.data;
+                var allFlights = response.data;
+                var selectedDesination;
+                var selectedDate;
+                var slectedSource = allFlights.filter((obj)=>obj.origin == $scope.src);
+                if(!$scope.dest){
+                    selectedDesination = slectedSource }
+                else {
+                   selectedDesination = slectedSource.filter((obj)=>obj.destination == $scope.dest);
+                }
+                if(!ctrl.myDate){
+                    selectedDate = selectedDesination }
+                else {
+                    selectedDate = selectedDesination.filter((obj)=>new Date(obj.date) >= ctrl.myDate);
+                }
+                $scope.flights = selectedDate;
                $scope.loading = false;
             });
     }
+
+    this.myDate = new Date();
+    this.isOpen = false;
 
 }]);

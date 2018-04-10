9d3d5102a80f43765a1dbdbfd290180e09404c19
pawan <pawanwebchamp@gmail.com>
2018-04-10 17:40:34 +0530

    Update functionality

--- app/js/app.module.js
+++ app/js/app.module.js
@@ -18,12 +18,14 @@ app.controller('flightController', ['$scope', '$http', function ($scope, $http) 
     $scope.SearchFlight = function () {
         
         $scope.searchResult = true;
+        $scope.flightinfo = true;
         $scope.loading = true;
         $http.get('app/flight/flight.json')
             .then(function (response) {
                 var allFlights = response.data;
                 var selectedDesination;
                 var selectedDate;
+                var returnsDate;
                 var slectedSource = allFlights.filter((obj)=>obj.origin == $scope.src);
                 if(!$scope.dest){
                     selectedDesination = slectedSource }
@@ -35,12 +37,59 @@ app.controller('flightController', ['$scope', '$http', function ($scope, $http) 
                 else {
                     selectedDate = selectedDesination.filter((obj)=>new Date(obj.date) >= ctrl.myDate);
                 }
-                $scope.flights = selectedDate;
+
+                if(!ctrl.returnDate){
+                    returnsDate = selectedDesination }
+                else {
+                    returnsDate = selectedDesination.filter((obj)=>new Date(obj.returndate) >= ctrl.returningDate);
+                }
+                
+                
+                $scope.flights = returnsDate;
                $scope.loading = false;
             });
     }
 
     this.myDate = new Date();
+    this.returningDate = new Date();
     this.isOpen = false;
+
+    // $scope.color = {
+    //     red: Math.floor(Math.random() * 255)
+    //   };
+    
+    //  $scope.disabled1 = Math.floor(Math.random() * 100);
+
+      $scope.cityList = ["Allahabad", "Bangalore", "Delhi", "Kolkata", "Pune", "Mumbai", "Varanasi"];
+      $scope.complete=function(string){
+          
+          var output=[];
+          angular.forEach($scope.cityList,function(src){
+              if(src.toLowerCase().indexOf(string.toLowerCase())>=0){
+                  output.push(src);
+              }
+          });
+          $scope.filterCity=output;
+      }
+      $scope.fillTextbox=function(string){
+          $scope.src=string;
+          $scope.filterCity=null;
+      }
+
+      $scope.returnCityList = ["Allahabad", "Bangalore", "Delhi", "Kolkata", "Pune", "Mumbai", "Varanasi"];
+      $scope.retcomplete=function(string){
+          
+          var output=[];
+          angular.forEach($scope.returnCityList,function(dest){
+              if(dest.toLowerCase().indexOf(string.toLowerCase())>=0){
+                  output.push(dest);
+              }
+          });
+          $scope.filterReturnCity=output;
+      }
+      $scope.returnfillTextbox=function(string){
+          $scope.dest=string;
+          $scope.filterReturnCity=null;
+      }
 
 }]);
